39c963a5f00fb86cb6462f931497954c81d4a3d9
jessler91 <justinessler@fastmail.com>
2020-08-27 20:10:21 -0500

    save

--- excel.js
+++ excel.js
@@ -1,10 +1,7 @@
-const Excel = require('exceljs');
-
+const Excel = require("exceljs");
 
 const workbook = new Excel.Workbook();
-
-const sheet = workbook.addWorksheet('Test Sheet 1');
-
+const sheet = workbook.addWorksheet("Test Sheet 1");
 
 // fetch sheet by name
-const worksheet = workbook.getWorksheet('Test Sheet 1');
+const worksheet = workbook.getWorksheet("Test Sheet 1");
